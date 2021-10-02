b1d20808dd41d34405b776a0462709ecfcf4b8f8
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2021-10-03 01:40:26 +0200

    added new components

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const options = [
+    'Strona Główna',
+    'Przystanki',
+    'Linie',
+    'Znajdź trasę'
+]
+
+const Nav = () => {
+
+    return (
+        <header>
+
+        </header>
+    );
+}
+
+export default Nav;
---
+++ src/components/NavButton/NavButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavButton = () => {
+    return (
+        <button></button>
+    );
+}
+
+export default NavButton;
