6f8b2a54d16dea0be1f56c3d4b920639b2006cf7
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2021-10-03 23:08:40 +0200

    prepared menu version for mobile devices

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter} from "react-router-dom";
 
 import NavButton from "../NavButton/NavButton";
@@ -13,23 +13,38 @@ const options = [
 
 const Nav = () => {
 
-    const buttons = options.map(option =>
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+    const buttons = options.map((option, id: number) =>
         <NavButton
+            id={id}
             key={option.name}
             name={option.name}
             url={option.url}
         />)
 
+    const handleSetShowMobileMenu = () => setShowMobileMenu(prev => !prev)
+
     return (
-        <header>
-            <BrowserRouter>
-                <nav>
-                    <ul className="main-menu">
-                        {buttons}
-                    </ul>
-                </nav>
-            </BrowserRouter>
-        </header>
+        <>
+            <div className="mobile-menu" onClick={handleSetShowMobileMenu}>
+                <svg width="40" height="36">
+                    <rect y="0" width="40" height="6" fill="#a1a1a1" className={showMobileMenu ? 'animation-upper-bar' : ''}  />
+                    <rect y="15" width="40" height="6" fill="#a1a1a1" className={showMobileMenu ? 'animation-middle-bar' : ''} />
+                    <rect y="30" width="40" height="6" fill="#a1a1a1" className={showMobileMenu ? 'animation-down-bar' : ''} />
+                </svg>
+            </div>
+            <header>
+                <BrowserRouter>
+                    <nav>
+                        <ul className="main-menu">
+                            {buttons}
+                        </ul>
+                    </nav>
+                </BrowserRouter>
+            </header>
+        </>
+
     );
 }
 
--- src/components/NavButton/NavButton.tsx
+++ src/components/NavButton/NavButton.tsx
@@ -5,12 +5,19 @@ import './NavButton.css'
 
 type propsType = {
     name: string,
-    url: string
+    url: string,
+    id: number
 }
 
-const NavButton = ({ name, url } : propsType) => {
+const NavButton = ({ name, url, id } : propsType) => {
+
+    const style = {
+        top: `${id*20}px`,
+        animationDelay: `${id}s`
+    }
+
     return (
-        <li className="menu-element">
+        <li className={"menu-element "} id={"menu-element-"+id} style={style}>
             <Link className="menu-link" to={url}>{name}</Link>
         </li>
 
