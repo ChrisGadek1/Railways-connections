4ecdb02e2c11098ad0587a5113b33ad408af0ec1
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2021-10-04 16:56:46 +0200

    added routing tests

---
+++ src/components/Content/Content.test.tsx
@@ -0,0 +1,20 @@
+import {fireEvent, render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from "../App/App";
+
+const renderData = [
+    {name: /Przystanki/i, testID: 'train-station-wrapper'},
+    {name: /Strona Główna/i, testID: 'main-page'},
+    {name: /Linie/i, testID: 'train-choosing-wrapper'},
+    {name: /Znajdź trasę/i, testID: 'connection-choosing-wrapper'}
+]
+
+it("renders valid component on menu link click", async () => {
+    render(<App />)
+    renderData.forEach( (testDetails:{name: RegExp, testID: string}) => {
+        const link = screen.getByRole('link', {name: testDetails.name});
+        fireEvent.click(link);
+        expect(screen.getByTestId(testDetails.testID))
+    })
+
+})
