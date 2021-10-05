6f8afab2f2e30928285cc84cfc4b45650d0e064d
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2021-10-05 14:15:13 +0200

    created main page description

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import './MainPage.css'
 
 const MainPage = () => {
     return(
-        <div data-testid="main-page">
-
-        </div>
+        <main data-testid="main-page" className="main-page">
+            <section>
+                <h1 className="main-page-title">Witaj na stronie <i className="icon-train"></i>Sieniczniańskich lini Kolejowych!</h1>
+                <p>
+                    Serwis ten umożliwia sprawdzenie wszystkich dostępnych tras oraz stacji kolejowych,
+                    jak również wyszukiwania tras pomiędzy przystankami
+                </p>
+            </section>
+        </main>
     )
 }
 
