1cdb71d5593ab8c633050d24cf9edc4ad2ca06c7
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2021-10-10 16:38:33 +0200

    added link to the Station component

--- src/components/StationComponent/StationComponent.tsx
+++ src/components/StationComponent/StationComponent.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Station from "../../data/classes/Station";
 import './StationComponent.css'
+import {Link, useHistory} from "react-router-dom";
 
 type propsType = {
     station: Station
 }
 
 const StationComponent = (props: propsType) => {
+
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push('/przystanki/'+props.station.id)
+    }
+
     return(
-        <div className="station-component">
+        <div className="station-component" onClick={handleClick}>
             <i className="icon-home"></i>
             <span>{props.station.name}</span>
         </div>
